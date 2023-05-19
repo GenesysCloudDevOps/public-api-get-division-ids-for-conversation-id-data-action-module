30d90de34fd60a807ac80aafeda85fe42e2e0bac
Charlie Conneely <charlie.conneely@genesys.com>
2023-05-19 16:33:21 +0100

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,45 @@
+resource "genesyscloud_integration_action" "action" {
+    name           = var.action_name
+    category       = var.action_category
+    integration_id = var.integration_id
+    secure         = var.secure_data_action
+    
+    contract_input  = jsonencode({
+        "additionalProperties" = true,
+        "properties" = {
+            "conversationId" = {
+                "type" = "string"
+            }
+        },
+        "type" = "object"
+    })
+    contract_output = jsonencode({
+        "additionalProperties" = true,
+        "properties" = {
+            "divisionIds" = {
+                "items" = {
+                    "type" = "string"
+                },
+                "type" = "array"
+            }
+        },
+        "type" = "object"
+    })
+    
+    config_request {
+        request_template     = "$${input.rawRequest}"
+        request_type         = "GET"
+        request_url_template = "/api/v2/conversations/$${input.conversationId}"
+        headers = {
+			Content-Type = "application/json"
+		}
+    }
+
+    config_response {
+        success_template = "{\"divisionIds\":$${divisionid}}"
+        translation_map = { 
+			divisionid = "$.divisions[*].division.id"
+		}
+               
+    }
+}
